b20116de7512897b90072f2817a6489b88e45e92
cqq <945327638@qq.com>
2023-06-30 09:44:13 +0800

    perf: 优化文案

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -2,7 +2,7 @@
   <div class="home-container">
     <van-button type="primary" size="large" to="/luckyMoneyOne">方式一: 组件写法</van-button>
     <van-button type="primary" size="large" to="/luckyMoneyTwo">方式二: 类的写法</van-button>
-    <van-button type="primary" size="large" to="/luckyMoneyRain">方式二: 监听红包雨</van-button>
+    <van-button type="primary" size="large" to="/luckyMoneyRain">方式三: 监听红包雨</van-button>
   </div>
 </template>
 <script setup lang="ts">
